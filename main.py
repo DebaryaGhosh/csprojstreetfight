c3e422ab7a0ccdf368932b4ed8d0599e078c4ef1
debaryaghosh <73695474+DebaryaGhosh@users.noreply.github.com>
2023-10-02 20:08:22 +0530

    intro screen added

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from game_data import *
 from support import *
 from debug import debug
 from game_updates import *
+from intro import run_intro
 
 pygame.init()
 
@@ -21,6 +22,7 @@ ui_data = initialize_ui()
 attack_data = initialize_attack_data()
 initialize_audio()
 
+current_state = screens['intro']
 
 def run_game(player_state, enemy_state, background_data, ui_data, attack_data):
     
@@ -28,17 +30,6 @@ def run_game(player_state, enemy_state, background_data, ui_data, attack_data):
     update_enemy(enemy_state, player_state, background_data, attack_data)
     update_background(background_data, player_state)
     update_ui(ui_data, [player_state, enemy_state, background_data])
-    
-
-# game loop
-while game_is_on:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game_is_on = False
-
-    screen.fill('black')
-
-    run_game(player_state, enemy_state, background_data, ui_data, attack_data)
 
     if background_data['bgshake']:
         player_state['offset'] = player_state['rect'].topleft - background_data['shake_offset'] + pygame.math.Vector2(0, 80)
@@ -49,9 +40,22 @@ while game_is_on:
 
     screen.blit(enemy_state['image'], enemy_state['offset'])
     screen.blit(player_state['image'], player_state['offset'])
-    #debug((enemy_state['is_attacking'], enemy_state['status'], enemy_state['attack_animation_playing'], enemy_state['attack_cooldown']))
-    debug((player_state['inair'], player_state['is_jumping']))
     
+
+# game loop
+while game_is_on:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_is_on = False
+
+    screen.fill('black')
+
+    if current_state == 'intro':
+        run_intro(ui_data, current_state)
+    elif current_state == 'game':
+        run_game(player_state, enemy_state, background_data, ui_data, attack_data)
+
+    debug((player_state['inair'], player_state['is_jumping']))
     
     pygame.display.update()
     clock.tick(FPS)
